fea5d43e464e74d8c1ede599f6d87a1cf93fbc45
katzeallergie <saito.t.av@m.titech.ac.jp>
2020-09-14 15:48:25 +0900

    add icons

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
@@ -41,11 +42,46 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Recommend" component={RecommendScreen} />
-        <Tab.Screen name="Shelf" component={ShelfScreen} />
-        <Tab.Screen name="Store" component={StoreScreen} />
+      <Tab.Navigator
+        tabBarOptions={{
+          activeTintColor: 'blue',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen 
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="ios-home" color={color} size={size} />
+            )
+          }} />
+        <Tab.Screen
+          name="Recommend"
+          component={RecommendScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="md-thumbs-up" color={color} size={size} />
+            )
+          }} />
+        <Tab.Screen
+          name="Shelf"
+          component={ShelfScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="ios-book" color={color} size={size} />
+            )
+          }} 
+          />
+        <Tab.Screen
+          name="Store"
+          component={StoreScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="ios-basket" color={color} size={size} />
+            )
+          }} 
+          />
       </Tab.Navigator>
     </NavigationContainer>
   );
